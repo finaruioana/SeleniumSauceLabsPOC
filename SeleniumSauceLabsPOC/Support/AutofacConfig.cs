86c6ba9fbdeb3172c98c20107605eecefd9bec54
finaruioana <il.finaru@gmail.com>
2016-11-16 11:10:06 +0000

    add Specflow.autofac and autofac registration

---
+++ SeleniumSauceLabsPOC/Support/AutofacConfig.cs
@@ -0,0 +1,13 @@
+﻿using Autofac;
+using SeleniumSauceLabsPOC.ConfigurationSettings;
+
+namespace SeleniumSauceLabsPOC.Support
+{
+    public class AutofacConfig : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.Register(c => new ConfigurationSettings.ConfigurationSettings()).As<IConfigurationSettings>().SingleInstance();
+        }
+    }
+}
---
+++ SeleniumSauceLabsPOC/Support/TestDependencies.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using Autofac;
+using SpecFlow.Autofac;
+using TechTalk.SpecFlow;
+
+namespace SeleniumSauceLabsPOC.Support
+{
+    public class TestDependencies
+    {
+        [ScenarioDependencies]
+        public static ContainerBuilder CreateContainerBuilder()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterModule(new AutofacConfig());
+
+            builder.RegisterTypes(typeof(TestDependencies).Assembly.GetTypes().Where(t => Attribute.IsDefined(t, typeof(BindingAttribute))).ToArray()).SingleInstance();
+            return builder;
+
+        }
+    }
+}
