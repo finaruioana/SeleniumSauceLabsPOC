0ed41677283a2921bdf90fe9888b1db1e264123d
finaruioana <il.finaru@gmail.com>
2016-11-25 09:54:16 +0000

    add config for mobile

--- SeleniumSauceLabsPOC/Browsers/BrowserFactory.cs
+++ SeleniumSauceLabsPOC/Browsers/BrowserFactory.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using OpenQA.Selenium;
+using OpenQA.Selenium.Appium.Android;
+using OpenQA.Selenium.Appium.iOS;
 using OpenQA.Selenium.Remote;
 using SeleniumSauceLabsPOC.ConfigurationSettings;
 using TechTalk.SpecFlow;
@@ -19,8 +21,11 @@ namespace SeleniumSauceLabsPOC.Browsers
             capabilities.SetCapability("name", ScenarioContext.Current.ScenarioInfo.Title);
             capabilities.SetCapability("username", settings.Username);
             capabilities.SetCapability("accessKey", settings.Key);
-
-            return new RemoteWebDriver(new Uri(settings.Server), capabilities);
+            if (!capabilities.HasCapability("platformName"))
+                return new RemoteWebDriver(new Uri(settings.Server), capabilities);
+            if( capabilities.GetCapability("platformName").Equals("Android"))
+                return new AndroidDriver<IWebElement>(new Uri(settings.Server), capabilities,new TimeSpan(0, 2, 0));
+                return new IOSDriver<IWebElement>(new Uri(settings.Server), capabilities, new TimeSpan(0, 2, 0));
         }
     }
 }
